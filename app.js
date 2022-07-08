ed0be7e11e8c03afe38d83afd7b626f9dc82dffa
Dat La <latandat117@gmail.com>
2022-07-08 18:05:58 +0700

    lab3.1

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+// app.set('view engine', 'pug')
+
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 // middleware
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/admin', adminRoutes)
+app.use('/admin', adminRoutes.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
