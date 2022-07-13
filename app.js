40b157fba16d6fcabe122e7974b62b58a292ccf9
Dat La <latandat117@gmail.com>
2022-07-13 17:29:44 +0700

    lab3.2-3.6

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-// app.set('view engine', 'pug')
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
@@ -16,7 +17,8 @@ app.use('/admin', adminRoutes.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', { pageTitle: 'Page Not Found'})
 })
 
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,9 @@ const router = express.Router()
 const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products)
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'})
 
 })
 
