9e7cbc038d04d1e11e141f6aa2ace55d7c95aa2c
Dat La <latandat117@gmail.com>
2022-07-13 18:05:59 +0700

    lab3.7-3.8

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,13 @@ const router = express.Router()
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    })
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
