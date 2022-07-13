629a9c051b919ad813d1e1b76b8396def30cef89
Dat La <latandat117@gmail.com>
2022-07-13 18:48:52 +0700

    work with ejs

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.engine( 
-    'hbs', 
-    expressHbs({
-        layoutDir: 'views/layouts/', 
-        defaultLayout: 'main-layout',
-        extname: 'hbs'
-    }))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
@@ -26,7 +19,7 @@ app.use(shopRoutes)
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.status(404).render('404', { pageTitle: 'Page Not Found'})
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: req.url})
 })
 
 app.listen(3000)
