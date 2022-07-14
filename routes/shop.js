2e374eca7b152b26eb6fa7e5ce1bd3dbc0b76215
Dat La <latandat117@gmail.com>
2022-07-14 18:31:23 +0700

    dynamic routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,19 @@
-const rootDir = require('../util/path')
-const path = require('path')
-const express = require('express')
+const path = require('path');
 
-const productsController = require('../controllers/products')
-const router = express.Router()
+const express = require('express');
 
-router.get('/', productsController.getProducts)
+const shopController = require('../controllers/shop');
 
-module.exports = router
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
